df2554b0ff1739981b68c118e92e8cdd0825e72f
lakshayystrideAhead <lakshay.saini@strideahead.in>
2024-08-22 22:50:01 +0530

    updated code with work experience section

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -7,7 +7,7 @@ const Experience = () => {
     <div className="py-20" id="experience">
       <h1 className="heading">
         My
-        <span className="text-purple">work experience.</span>
+        <span className="text-purple"> work experience.</span>
       </h1>
       <div className="w-full mt-12 grid lg:grid-cols-4 grid-cols-1 gap-10">
         {workExperience.map((card) => (
@@ -23,6 +23,14 @@ const Experience = () => {
                 alt={card.thumbnail}
                 className="lg:w-32 md:w-20 w-16"
               />
+              <div className="lg:ms-5">
+                <h1 className="text-start text-xl md:text-2xl font-bold">
+                  {card.title}
+                </h1>
+                <p className="text-start text-white-100 mt-3 font-semibold">
+                  {card.desc}
+                </p>
+              </div>
             </div>
           </Button>
         ))}

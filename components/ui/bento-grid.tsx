25a7e7dbe0a8aa5dc0d5f215a8386c1a730238e7
lakshayystrideAhead <lakshay.saini@strideahead.in>
2024-09-04 21:33:13 +0530

    globe added and moving skills added in grid

--- components/ui/bento-grid.tsx
+++ components/ui/bento-grid.tsx
@@ -45,12 +45,11 @@ export const BentoGridItem = ({
       }}
     >
       {header}
-      <div className="group-hover/bento:translate-x-2 transition duration-200">
-        {icon}
-        <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
+      <div className="group-hover/bento:translate-x-2 transition duration-200 z-10">
+        <div className="font-sans text-xl font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
           {title}
         </div>
-        <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
+        <div className="font-sans text-base font-normal text-neutral-600 dark:text-neutral-300">
           {description}
         </div>
       </div>

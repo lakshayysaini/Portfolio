9415599c79de80a62b03a3bea3a42921ca727173
lakshayystrideAhead <lakshay.saini@strideahead.in>
2024-09-24 20:56:47 +0530

    updated code wiht desktop completed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,11 +9,11 @@ const Footer = () => {
 
   return (
     <footer className="w-full pt-20 pb-10" id="contact">
-      <div className="w-full absolute left-0 -bottom-72 min-h-96">
+      <div className="w-full absolute left-0 -bottom-72 min-h-96 z-0">
         <img
           src="/footer-grid.svg"
           alt="grid"
-          className="w-full h-full opacity-50"
+          className="w-full h-full opacity-50 "
         />
       </div>
 

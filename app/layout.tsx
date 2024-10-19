43503c70244b845d2ab2393576d939c668c28012
lakshayystrideAhead <lakshay.saini@strideahead.in>
2024-10-19 09:55:57 +0530

    updated code with meta name changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Lakshayyy Portfolio",
+  title: "Lakshayyy's Portfolio",
   description: "Made with Love",
 };
 
